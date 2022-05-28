7182c76ed742589342b2c180db6f564ba540b381
Vladimir Mironiuk <vladimironiuk@gmail.com>
2022-05-28 12:52:43 +0300

    GameInteractor.start() does not invoke if there are no robots

--- Robowars/RobowarsTests/LaunchGameClientUseCaseTests.swift
+++ Robowars/RobowarsTests/LaunchGameClientUseCaseTests.swift
@@ -7,14 +7,29 @@
 
 import XCTest
 
+protocol Robot {}
+
 final class GameInteractor {
+    var firstRobot: Robot?
+    var secondRobot: Robot?
     var startCallCount: Int = .zero
+    
+    func start() {
+        guard firstRobot != nil, secondRobot != nil else { return }
+        startCallCount += 1
+    }
 }
 
 class LaunchGameClientUseCaseTests: XCTestCase {
     
     func test_start_doesNotInvokesOnGameInteractorInit() {
         let sut = GameInteractor()
+        XCTAssertEqual(sut.startCallCount, .zero)
+    }
+    
+    func test_start_doesNotInvokeIfThereAreNoRobots() {
+        let sut = GameInteractor()
+        sut.start()
         XCTAssertEqual(sut.startCallCount, .zero)
     }
 }
