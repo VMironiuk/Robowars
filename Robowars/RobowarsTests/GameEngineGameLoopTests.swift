e438f315d9059d4b4c1b1bc1016f06ee30570aa4
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2022-07-20 09:47:29 +0300

    Make more realistic game tests

--- Robowars/RobowarsTests/GameEngineGameLoopTests.swift
+++ Robowars/RobowarsTests/GameEngineGameLoopTests.swift
@@ -36,6 +36,32 @@ class GameEngineGameLoopTests: XCTestCase {
         XCTAssertTrue(sut.winner === secondRobot)
     }
     
+    func test_gameEngine_gameLoopForMoreRealisticGameFinishedWithFirstRobotAsWinner() {
+        // Given
+        let firstRobot = ShootingTestRobot(ships: makeShipsInGoodWay(), maxBattlefieldSize: 10)
+        let secondRobot = ShootingTestRobot(ships: makeShipsInGoodWay(), maxBattlefieldSize: 10)
+        let sut = GameEngine(shipsValidator: DummyShipsValidator())
+        sut.setFirstRobot(firstRobot)
+        sut.setSecondRobot(secondRobot)
+        // When
+        sut.start()
+        // Then
+        XCTAssertTrue(sut.winner === firstRobot)
+    }
+    
+    func test_gameEngine_gameLoopForMoreRealisticGameFinishedWithSecondRobotAsWinner() {
+        // Given
+        let firstRobot = ShootingTestRobot(ships: makeShipsInBadWay(), maxBattlefieldSize: 10)
+        let secondRobot = ShootingTestRobot(ships: makeShipsInGoodWay(), maxBattlefieldSize: 10)
+        let sut = GameEngine(shipsValidator: DummyShipsValidator())
+        sut.setFirstRobot(firstRobot)
+        sut.setSecondRobot(secondRobot)
+        // When
+        sut.start()
+        // Then
+        XCTAssertTrue(sut.winner === secondRobot)
+    }
+
     // Helpers
     
     private final class ShootingTestRobot: Robot {
@@ -70,5 +96,35 @@ class GameEngineGameLoopTests: XCTestCase {
         func shootResult(_ result: ShootResult, for coordinate: CGPoint) {
             // Can be empty for these tests
         }
+    }
+    
+    private func makeShipsInGoodWay() -> [CGRect] {
+        [
+            CGRect(x: 0, y: 0, width: 1, height: 4),
+            CGRect(x: 0, y: 5, width: 1, height: 3),
+            CGRect(x: 0, y: 9, width: 3, height: 1),
+            CGRect(x: 2, y: 0, width: 2, height: 1),
+            CGRect(x: 5, y: 0, width: 2, height: 1),
+            CGRect(x: 4, y: 9, width: 2, height: 1),
+            CGRect(x: 2, y: 2, width: 1, height: 1),
+            CGRect(x: 2, y: 4, width: 1, height: 1),
+            CGRect(x: 2, y: 7, width: 1, height: 1),
+            CGRect(x: 7, y: 5, width: 1, height: 1),
+        ]
+    }
+    
+    private func makeShipsInBadWay() -> [CGRect] {
+        [
+            CGRect(x: 0, y: 0, width: 4, height: 1),
+            CGRect(x: 5, y: 0, width: 3, height: 1),
+            CGRect(x: 9, y: 0, width: 1, height: 1),
+            CGRect(x: 0, y: 2, width: 3, height: 1),
+            CGRect(x: 4, y: 2, width: 2, height: 1),
+            CGRect(x: 7, y: 2, width: 2, height: 1),
+            CGRect(x: 0, y: 4, width: 2, height: 1),
+            CGRect(x: 3, y: 4, width: 1, height: 1),
+            CGRect(x: 5, y: 4, width: 1, height: 1),
+            CGRect(x: 7, y: 4, width: 1, height: 1),
+        ]
     }
 }
