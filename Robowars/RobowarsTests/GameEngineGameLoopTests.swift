6c0e504ad8d229cb5bf313218415a57feb35cbc6
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2022-07-20 09:22:48 +0300

    Specify arrays of ships for game loop tests instead of just a single ship

--- Robowars/RobowarsTests/GameEngineGameLoopTests.swift
+++ Robowars/RobowarsTests/GameEngineGameLoopTests.swift
@@ -12,8 +12,8 @@ class GameEngineGameLoopTests: XCTestCase {
     
     func test_gameEngine_gameLoopFinishedWithFirstRobotAsWinner() {
         // Given
-        let firstRobot = ShootingTestRobot(ship: CGRect(x: 1, y: 1, width: 1, height: 1), maxBattlefieldSize: 2)
-        let secondRobot = ShootingTestRobot(ship: CGRect(x: 1, y: 1, width: 1, height: 1), maxBattlefieldSize: 2)
+        let firstRobot = ShootingTestRobot(ships: [CGRect(x: 1, y: 1, width: 1, height: 1)], maxBattlefieldSize: 2)
+        let secondRobot = ShootingTestRobot(ships: [CGRect(x: 1, y: 1, width: 1, height: 1)], maxBattlefieldSize: 2)
         let sut = GameEngine(shipsValidator: DummyShipsValidator())
         sut.setFirstRobot(firstRobot)
         sut.setSecondRobot(secondRobot)
@@ -25,8 +25,8 @@ class GameEngineGameLoopTests: XCTestCase {
     
     func test_gameEngine_gameLoopFinishedWithSecondRobotAsWinner() {
         // Given
-        let firstRobot = ShootingTestRobot(ship: CGRect(x: 2, y: 0, width: 1, height: 1), maxBattlefieldSize: 2)
-        let secondRobot = ShootingTestRobot(ship: CGRect(x: 1, y: 1, width: 1, height: 1), maxBattlefieldSize: 2)
+        let firstRobot = ShootingTestRobot(ships: [CGRect(x: 2, y: 0, width: 1, height: 1)], maxBattlefieldSize: 2)
+        let secondRobot = ShootingTestRobot(ships: [CGRect(x: 1, y: 1, width: 1, height: 1)], maxBattlefieldSize: 2)
         let sut = GameEngine(shipsValidator: DummyShipsValidator())
         sut.setFirstRobot(firstRobot)
         sut.setSecondRobot(secondRobot)
@@ -41,15 +41,15 @@ class GameEngineGameLoopTests: XCTestCase {
     private final class ShootingTestRobot: Robot {
         private var shootPoint: CGPoint = .zero
         private let maxBattlefieldSize: CGFloat
-        private let ship: CGRect
+        private let _ships: [CGRect]
         
-        init(ship: CGRect, maxBattlefieldSize: CGFloat) {
-            self.ship = ship
+        init(ships: [CGRect], maxBattlefieldSize: CGFloat) {
+            self._ships = ships
             self.maxBattlefieldSize = maxBattlefieldSize
         }
         
         var ships: [CGRect] {
-            [ship]
+            _ships
         }
         
         func set(battlefield: CGRect, ships: [CGSize]) {
