7ac3b4e011e735991c7e80452e2f2eeeab5eda9d
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2022-08-17 08:45:24 +0300

    Add an empty yet prototype app

---
+++ Prototype/Prototype/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Prototype
+//
+//  Created by Volodymyr Myroniuk on 17.08.2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
