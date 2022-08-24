ecee79141cf0c941758bade491eb96ddfc917993
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2022-08-24 08:09:02 +0300

    Place battlefield views

--- Prototype/Prototype/MainViewController.swift
+++ Prototype/Prototype/MainViewController.swift
@@ -10,11 +10,19 @@ import Cocoa
 class MainViewController: NSViewController {
     
     @IBOutlet private weak var errorView: NSView!
+    @IBOutlet private weak var firstRobotBattlefieldView: NSView!
+    @IBOutlet private weak var secondRobotBattlefieldView: NSView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         errorView.wantsLayer = true
         errorView.layer?.backgroundColor = NSColor(named: "ErrorViewColor")?.cgColor
+        
+        firstRobotBattlefieldView.wantsLayer = true
+        firstRobotBattlefieldView.layer?.backgroundColor = NSColor.cyan.cgColor
+        
+        secondRobotBattlefieldView.wantsLayer = true
+        secondRobotBattlefieldView.layer?.backgroundColor = NSColor.cyan.cgColor
     }
 }
