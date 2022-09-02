7a335812d93b5ef99e5e2c4711b7afb889296b92
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2022-09-02 08:44:21 +0300

    Make rowsCount and columnsCount non-static properties

--- Prototype/Prototype/BattlefieldView.swift
+++ Prototype/Prototype/BattlefieldView.swift
@@ -9,8 +9,8 @@ import Cocoa
 
 class BattlefieldView: NSView {
     
-    private static let rowsCount: CGFloat = 10
-    private static let columnsCount: CGFloat = 10
+    private let rowsCount: CGFloat = 10
+    private let columnsCount: CGFloat = 10
     
     override init(frame frameRect: NSRect) {
         super.init(frame: frameRect)
@@ -25,28 +25,16 @@ class BattlefieldView: NSView {
     override func layout() {
         super.layout()
         
-        let tileWidth: CGFloat = (frame.width - Self.columnsCount - 1) / Self.columnsCount
-        let tileHeight: CGFloat = (frame.height - Self.rowsCount - 1) / Self.rowsCount
+        let tileWidth: CGFloat = (frame.width - columnsCount - 1) / columnsCount
+        let tileHeight: CGFloat = (frame.height - rowsCount - 1) / rowsCount
         var y: CGFloat = 1
         var x: CGFloat = 1
 
-        for row in .zero..<Int(Self.rowsCount) {
-            for column in .zero..<Int(Self.columnsCount) {
+        for row in .zero..<Int(rowsCount) {
+            for column in .zero..<Int(columnsCount) {
                 let tile = subviews[row * 10 + column]
                 tile.frame = NSRect(x: x, y: y, width: tileWidth, height: tileHeight)
                 x += tileWidth + 1
-                
-                if row == 0 && column == 0 || row == 2 && column == 1 || row == 4 && column == 6 {
-                    tile.layer?.backgroundColor = NSColor(named: "MissedShotColor")?.cgColor
-                }
-                
-                if row == 1 && column == 1 {
-                    tile.layer?.backgroundColor = NSColor(named: "DamagedShipColor")?.cgColor
-                }
-                
-                if row == 1 && column == 2 || row == 1 && column == 3 || row == 1 && column == 4{
-                    tile.layer?.backgroundColor = NSColor(named: "ShipColor")?.cgColor
-                }
             }
             y += tileHeight + 1
             x = 1
@@ -57,7 +45,7 @@ class BattlefieldView: NSView {
         wantsLayer = true
         layer?.backgroundColor = NSColor(named: "Window Background Color")?.cgColor
         
-        for _ in .zero..<Int(Self.rowsCount * Self.columnsCount) {
+        for _ in .zero..<Int(rowsCount * columnsCount) {
             let tile = NSView()
             tile.wantsLayer = true
             tile.layer?.backgroundColor = NSColor(named: "BattlefieldColor")?.cgColor
