f7cbcb767bb3ffdc599013b8e90e40d807ef36b0
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2022-09-03 09:55:07 +0300

    Place ships and shots from a real game

--- Prototype/Prototype/BattlefieldView.swift
+++ Prototype/Prototype/BattlefieldView.swift
@@ -7,21 +7,30 @@
 
 import Cocoa
 
+struct BattlefieldViewState {
+    let hitShots: [CGPoint]
+    let missedShots: [CGPoint]
+    let ships: [CGPoint]
+}
+
 class BattlefieldView: NSView {
     
     private let rowsCount: CGFloat = 10
     private let columnsCount: CGFloat = 10
+    private var state: BattlefieldViewState
     
     override var isFlipped: Bool {
         true
     }
     
     override init(frame frameRect: NSRect) {
+        state = BattlefieldViewState(hitShots: [], missedShots: [], ships: [])
         super.init(frame: frameRect)
         setup()
     }
     
     required init?(coder: NSCoder) {
+        state = BattlefieldViewState(hitShots: [], missedShots: [], ships: [])
         super.init(coder: coder)
         setup()
     }
@@ -39,10 +48,27 @@ class BattlefieldView: NSView {
                 let tile = subviews[row * 10 + column]
                 tile.frame = NSRect(x: x, y: y, width: tileWidth, height: tileHeight)
                 x += tileWidth + 1
+                
+                if state.missedShots.contains(CGPoint(x: column, y: row)) {
+                    tile.layer?.backgroundColor = NSColor(named: "MissedShotColor")?.cgColor
+                }
+                
+                if state.hitShots.contains(CGPoint(x: column, y: row)) {
+                    tile.layer?.backgroundColor = NSColor(named: "DamagedShipColor")?.cgColor
+                }
+                
+                if state.ships.contains(CGPoint(x: column, y: row)) {
+                    tile.layer?.backgroundColor = NSColor(named: "ShipColor")?.cgColor
+                }
             }
             y += tileHeight + 1
             x = 1
         }
+    }
+    
+    func update(state: BattlefieldViewState) {
+        self.state = state
+        layoutSubtreeIfNeeded()
     }
     
     private func setup() {
--- Prototype/Prototype/MainViewController.swift
+++ Prototype/Prototype/MainViewController.swift
@@ -18,5 +18,57 @@ class MainViewController: NSViewController {
         
         errorView.wantsLayer = true
         errorView.layer?.backgroundColor = NSColor(named: "ErrorViewColor")?.cgColor
+        
+        firstRobotBattlefieldView.update(state: firstRobotState())
+        secondRobotBattlefieldView.update(state: secondRobotState())
+    }
+    
+    private func firstRobotState() -> BattlefieldViewState {
+        BattlefieldViewState(
+            hitShots: [
+                CGPoint(x: 0, y: 0), CGPoint(x: 0, y: 1), CGPoint(x: 0, y: 2), CGPoint(x: 0, y: 3),
+                CGPoint(x: 0, y: 5), CGPoint(x: 0, y: 6), CGPoint(x: 0, y: 7), CGPoint(x: 0, y: 9),
+                CGPoint(x: 1, y: 9), CGPoint(x: 2, y: 0), CGPoint(x: 3, y: 0), CGPoint(x: 4, y: 0),
+                CGPoint(x: 6, y: 0), CGPoint(x: 7, y: 0), CGPoint(x: 9, y: 4), CGPoint(x: 3, y: 6),
+                CGPoint(x: 7, y: 7), CGPoint(x: 3, y: 9), CGPoint(x: 4, y: 9)
+            ],
+            missedShots: [
+                CGPoint(x: 9, y: 0), CGPoint(x: 3, y: 1), CGPoint(x: 4, y: 1), CGPoint(x: 5, y: 1),
+                CGPoint(x: 6, y: 1), CGPoint(x: 8, y: 1), CGPoint(x: 9, y: 1), CGPoint(x: 2, y: 2),
+                CGPoint(x: 2, y: 3), CGPoint(x: 6, y: 2), CGPoint(x: 8, y: 2), CGPoint(x: 9, y: 2),
+                CGPoint(x: 2, y: 3), CGPoint(x: 5, y: 3), CGPoint(x: 6, y: 3), CGPoint(x: 2, y: 4),
+                CGPoint(x: 3, y: 4), CGPoint(x: 5, y: 4), CGPoint(x: 6, y: 4), CGPoint(x: 7, y: 4),
+                CGPoint(x: 1, y: 5), CGPoint(x: 2, y: 5), CGPoint(x: 4, y: 5), CGPoint(x: 6, y: 5),
+                CGPoint(x: 7, y: 5), CGPoint(x: 4, y: 6), CGPoint(x: 5, y: 7), CGPoint(x: 8, y: 7),
+                CGPoint(x: 0, y: 8), CGPoint(x: 8, y: 8), CGPoint(x: 8, y: 9), CGPoint(x: 9, y: 9),
+            ],
+            ships: [
+                CGPoint(x: 4, y: 3)
+            ]
+        )
+    }
+    
+    private func secondRobotState() -> BattlefieldViewState {
+        BattlefieldViewState(
+            hitShots: [
+                CGPoint(x: 0, y: 0), CGPoint(x: 1, y: 0), CGPoint(x: 2, y: 0), CGPoint(x: 4, y: 0),
+                CGPoint(x: 1, y: 3), CGPoint(x: 4, y: 2), CGPoint(x: 4, y: 3), CGPoint(x: 4, y: 4),
+                CGPoint(x: 4, y: 5), CGPoint(x: 9, y: 5), CGPoint(x: 0, y: 6), CGPoint(x: 1, y: 6),
+                CGPoint(x: 3, y: 7), CGPoint(x: 4, y: 7), CGPoint(x: 7, y: 8), CGPoint(x: 8, y: 8),
+                CGPoint(x: 9, y: 8), CGPoint(x: 0, y: 9), CGPoint(x: 1, y: 9), CGPoint(x: 4, y: 9)
+            ],
+            missedShots: [
+                CGPoint(x: 6, y: 0), CGPoint(x: 9, y: 0), CGPoint(x: 0, y: 1), CGPoint(x: 4, y: 1),
+                CGPoint(x: 4, y: 1), CGPoint(x: 5, y: 1), CGPoint(x: 6, y: 1), CGPoint(x: 7, y: 1),
+                CGPoint(x: 8, y: 1), CGPoint(x: 3, y: 2), CGPoint(x: 5, y: 2), CGPoint(x: 6, y: 2),
+                CGPoint(x: 7, y: 2), CGPoint(x: 6, y: 3), CGPoint(x: 9, y: 3), CGPoint(x: 0, y: 4),
+                CGPoint(x: 6, y: 4), CGPoint(x: 7, y: 4), CGPoint(x: 8, y: 4), CGPoint(x: 6, y: 5),
+                CGPoint(x: 7, y: 5), CGPoint(x: 8, y: 5), CGPoint(x: 6, y: 6), CGPoint(x: 7, y: 6),
+                CGPoint(x: 8, y: 6), CGPoint(x: 9, y: 6), CGPoint(x: 0, y: 7), CGPoint(x: 5, y: 7),
+                CGPoint(x: 4, y: 8), CGPoint(x: 6, y: 8), CGPoint(x: 3, y: 9), CGPoint(x: 5, y: 9),
+                CGPoint(x: 9, y: 9)
+            ],
+            ships: []
+        )
     }
 }
