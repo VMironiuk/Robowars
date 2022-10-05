0ac23af5886fd1157acdb3bf9a5839573f77efac
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2022-10-05 09:24:24 +0300

    Place all projects to the workspace

---
+++ RobowarsClient-macOS/RobowarsClient-macOS/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  RobowarsClient-macOS
+//
+//  Created by Volodymyr Myroniuk on 05.10.2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
