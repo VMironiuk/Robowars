cf86e5231d5c683fd4b08c7c551cb3bf267fdaf2
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2022-10-14 08:15:45 +0300

    Remove unnecessary tests

--- RobowarsClient-macOS/RobowarsClient-macOSTests/SidebarViewControllerTests.swift
+++ RobowarsClient-macOS/RobowarsClient-macOSTests/SidebarViewControllerTests.swift
@@ -65,41 +65,6 @@ final class SidebarViewControllerTests: XCTestCase {
         XCTAssertEqual(delegate.gameModeDidChangeCallCount, 1)
     }
     
-    func test_sidebarVC_informsItsDelegateAboutManualGameModesChanges() {
-        let delegate = SidebarSpy()
-        let chooseModeVC = ChooseGameModeViewController(gameModes: [.classic, .flyweight])
-        let sut = makeSUT(
-            chooseRobotsViewController: ChooseRobotsViewController(robots: [DummyRobot()]),
-            chooseGameModeViewController: chooseModeVC,
-            delegate: delegate)
-        
-        _ = sut.view
-        _ = chooseModeVC.view
-        chooseModeVC.gameModeComboBox.selectItem(at: 1)
-
-        XCTAssertEqual(delegate.firstRobotDidChangeCallCount, 1)
-        XCTAssertEqual(delegate.secondRobotDidChangeCallCount, 1)
-        XCTAssertEqual(delegate.gameModeDidChangeCallCount, 2)
-    }
-    
-    func test_sidebarVC_informsItsDelegateAboutManualRobotsChanges() {
-        let delegate = SidebarSpy()
-        let chooseRobotsVC = ChooseRobotsViewController(robots: [DummyRobot(), DummyRobot(), DummyRobot()])
-        let sut = makeSUT(
-            chooseRobotsViewController: chooseRobotsVC,
-            chooseGameModeViewController: ChooseGameModeViewController(gameModes: [.classic, .flyweight]),
-            delegate: delegate)
-        
-        _ = sut.view
-        _ = chooseRobotsVC.view
-        chooseRobotsVC.firstRobotComboBox.selectItem(at: 1)
-        chooseRobotsVC.secondRobotComboBox.selectItem(at: 2)
-
-        XCTAssertEqual(delegate.firstRobotDidChangeCallCount, 2)
-        XCTAssertEqual(delegate.secondRobotDidChangeCallCount, 2)
-        XCTAssertEqual(delegate.gameModeDidChangeCallCount, 1)
-    }
-    
     func test_sidebarVC_informsItsDelegateAboutManualRobotsAndGameModeChanges() {
         let delegate = SidebarSpy()
         let chooseRobotsVC = ChooseRobotsViewController(robots: [DummyRobot(), DummyRobot(), DummyRobot()])
