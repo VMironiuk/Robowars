1df1f1abc5da37a95d15d511ab61eba562e1510a
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2022-10-25 08:23:54 +0300

    Replace view controller with split view controller

---
+++ RobowarsClient-macOS/RobowarsClient-macOS/SplitViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SplitViewController.swift
+//  RobowarsClient-macOS
+//
+//  Created by Volodymyr Myroniuk on 25.10.2022.
+//
+
+import Cocoa
+
+class SplitViewController: NSSplitViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
