76bcf22c4c0ae34eb08a049ffa8cc8adba986720
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2022-11-09 09:11:47 +0200

    Compose sidebar and main view inside split view controller

--- RobowarsClient-macOS/RobowarsClient-macOS/SplitViewController.swift
+++ RobowarsClient-macOS/RobowarsClient-macOS/SplitViewController.swift
@@ -6,10 +6,36 @@
 //
 
 import Cocoa
+import Robowars
 
 class SplitViewController: NSSplitViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let sidebarVC = SidebarViewController(
+            chooseRobotsViewController: ChooseRobotsViewController(robots: []),
+            chooseGameModeViewController: ChooseGameModeViewController(gameModes: []),
+            delegate: self
+        )
+        
+        let mainVC = MainViewController(
+            firstBattlefieldViewController: BattlefieldViewController(),
+            secondBattlefieldViewController: BattlefieldViewController()
+        )
+        
+        addSplitViewItem(NSSplitViewItem(sidebarWithViewController: sidebarVC))
+        addSplitViewItem(NSSplitViewItem(viewController: mainVC))
+    }
+}
+
+extension SplitViewController: SidebarViewControllerDelegate {
+    func sidebarViewController(_ viewController: SidebarViewController, firstRobotDidChange robot: RobotProtocol) {
+    }
+    
+    func sidebarViewController(_ viewController: SidebarViewController, secondRobotDidChange robot: RobotProtocol) {
+    }
+    
+    func sidebarViewController(_ viewController: SidebarViewController, gameModeDidChange gameMode: GameMode) {
     }
 }
