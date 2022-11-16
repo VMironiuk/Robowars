54fb561b138943fd4248cefd393cd37ee4dea2ad
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2022-11-16 08:19:27 +0200

    Hardcode ships for Misfire robot

--- RobowarsClient-macOS/RobowarsClient-macOS/Robots/Misfire.swift
+++ RobowarsClient-macOS/RobowarsClient-macOS/Robots/Misfire.swift
@@ -10,7 +10,18 @@ import Robowars
 
 final class Misfire: RobotProtocol {
     var ships: [CGRect] {
-        []
+        [
+            CGRect(x: 0, y: 0, width: 1, height: 4),
+            CGRect(x: 0, y: 5, width: 1, height: 3),
+            CGRect(x: 0, y: 9, width: 2, height: 1),
+            CGRect(x: 2, y: 0, width: 3, height: 1),
+            CGRect(x: 6, y: 0, width: 2, height: 1),
+            CGRect(x: 9, y: 4, width: 1, height: 1),
+            CGRect(x: 3, y: 6, width: 1, height: 1),
+            CGRect(x: 7, y: 7, width: 1, height: 1),
+            CGRect(x: 3, y: 9, width: 2, height: 1),
+            CGRect(x: 4, y: 3, width: 1, height: 1)
+        ]
     }
     
     var name: String {
