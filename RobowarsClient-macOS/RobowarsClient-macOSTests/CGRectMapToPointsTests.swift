5c76334c897a2dc09dff11cc77cc9cfaa58c91b5
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2022-11-19 10:05:48 +0200

    Prove that the [CGRect].mapToPoints() method returns an array points with correct coordinates for 1x1 ships

--- RobowarsClient-macOS/RobowarsClient-macOSTests/CGRectMapToPointsTests.swift
+++ RobowarsClient-macOS/RobowarsClient-macOSTests/CGRectMapToPointsTests.swift
@@ -20,4 +20,21 @@ final class CGRectMapToPointsTests: XCTestCase {
         
         XCTAssertEqual(ships.mapToPoints(), [CGPoint(x: 5, y: 6)])
     }
+    
+    func test_returnsPointsWithCorrectCoordinatesFor1x1Ships() {
+        let ships: [CGRect] = [
+            CGRect(x: 5, y: 6, width: 1, height: 1),
+            CGRect(x: 0, y: 0, width: 1, height: 1),
+            CGRect(x: 9, y: 1, width: 1, height: 1),
+            CGRect(x: 7, y: 5, width: 1, height: 1)
+        ]
+        let expectedPoints: [CGPoint] = [
+            CGPoint(x: 5, y: 6),
+            CGPoint(x: 0, y: 0),
+            CGPoint(x: 9, y: 1),
+            CGPoint(x: 7, y: 5)
+        ]
+        
+        XCTAssertEqual(ships.mapToPoints(), expectedPoints)
+    }
 }
