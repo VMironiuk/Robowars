7532301d6a117ada1939277c3c13ca5c15b65bbc
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2022-11-22 20:21:30 +0200

    Fix a typo

--- RobowarsClient-macOS/RobowarsClient-macOSTests/CGRectMapToPointsTests.swift
+++ RobowarsClient-macOS/RobowarsClient-macOSTests/CGRectMapToPointsTests.swift
@@ -36,14 +36,14 @@ final class CGRectMapToPointsTests: XCTestCase {
         XCTAssertEqual([ship].mapToPoints(), expectedPoints)
     }
     
-    func test_returnsPointsWithCorrectCoordinateFor2x1Ship() {
+    func test_returnsPointsWithCorrectCoordinatesFor2x1Ship() {
         let ship: CGRect = CGRect(x: 0, y: 0, width: 2, height: 1)
         let expectedPoints: [CGPoint] = [CGPoint(x: 0, y: 0), CGPoint(x: 1, y: 0)]
         
         XCTAssertEqual([ship].mapToPoints(), expectedPoints)
     }
     
-    func test_returnsPointsWithCorrectCoordinateFor1x2Ship() {
+    func test_returnsPointsWithCorrectCoordinatesFor1x2Ship() {
         let ship: CGRect = CGRect(x: 0, y: 0, width: 1, height: 2)
         let expectedPoints: [CGPoint] = [CGPoint(x: 0, y: 0), CGPoint(x: 0, y: 1)]
         
